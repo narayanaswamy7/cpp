a15e9a530be6afdca88d0561b924ebb78026f5ed
narayana.swamy <nswamy751@gmail.com>
2021-06-21 21:20:58 +0530

    code chef june cook off solutions

---
+++ codechef/COOK130C/TheWave.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main() {
+	int n, q;
+	cin>>n>>q;
+	ll a[n];
+	
+	for(int i = 0; i < n; i++) {
+		cin>>a[i];
+	}
+	
+	sort(a, a+n, greater<int>());
+	ll x;
+	
+	int jump = sqrt(n);
+	
+	while(q--) {
+		cin>>x;
+		int pos = 0;
+		while(pos+jump < n && a[pos+jump] > x) pos+=jump;
+		while(pos<n && a[pos] > x)	pos++;
+		
+		if(a[pos] == x)
+			cout<<"0"<<"\n";
+		else if(pos%2==0)
+			cout<<"POSITIVE"<<"\n";
+		else
+		cout<<"NEGATIVE"<<"\n";
+	}
+	
+	return 0;
+}
---
+++ codechef/README.md
@@ -0,0 +1,1 @@
+# Code Chef June Cook Off 2021
